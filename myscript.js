dbc6530b1d165121a22b864bf1fc9f83457fe3a2
Robert Lowe <robertmlowe@rmlowe.com>
2018-09-04 04:48:49 +0800

    JS Level One - Part Seven For Loops

--- myscript.js
+++ myscript.js
@@ -1,16 +1,6 @@
-var x = 1;
+// FOR LOOPS
+var word = "ABABABABAB"
 
-// Write a while loop that prints out
-// only the even numbers from 1 to 10.
-
-while (x <= 10) {
-  //console.log("x is currently: "+x);
-
-  if (x % 2 === 0) {
-    console.log("x is: " + x);
-  }
-
-  //console.log("x is still less than 5, adding 1 to x");
-
-  x = x+1;
+for (var i = 0; i < word.length; i=i+2) {
+  console.log(word[i]);
 }
