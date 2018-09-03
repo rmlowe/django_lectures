54edafc686ca8d16fb96567f0f10a7348e020679
Robert Lowe <robertmlowe@rmlowe.com>
2018-09-04 04:00:53 +0800

    JS Level One - Part Five - Control Flow

--- myscript.js
+++ myscript.js
@@ -1,3 +1,15 @@
-var lbs = prompt("What is the weight in pounds (lbs)?");
-alert("That is: " + (lbs * 0.454) + " kilograms");
-console.log("Conversion Completed");
+
+var ham = 0;
+var cheese = 0;
+
+var report = "blank";
+
+if (ham >= 10 && cheese >= 10) {
+  report = "Strong sales of both ham and cheese!"
+}else if (ham === 0 && cheese === 0) {
+  report = "Nothing Sold!"
+}else {
+  report = "We had some sales of items"
+}
+
+console.log(report);
